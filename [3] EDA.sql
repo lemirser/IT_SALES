75858e59a54c99a0b9409e73b9adb872cd032048
lemirser <serzoleandro@gmail.com>
2025-02-06 02:05:36 +0800

    Remove honorifics and professional degree in customer_name column

--- [3] EDA.sql
+++ [3] EDA.sql
@@ -1,4 +1,4 @@
--- Version 1.04, Last Modified: 2025-02-05
+-- Version 1.05, Last Modified: 2025-02-06
 -- This script is Exploratory Data Analysis (EDA).
 
 
@@ -104,8 +104,7 @@ LIMIT 10;
  * The query will add row numbers as new column, if the there are any duplicate/s, the value will be >1.
  * We limit the result since we only need to fetch values that are greater than 1.
  * If there are specific rules for each tables, we can use it in the 'partition by'.
- * For example, if there is a data validation in the customers table by only registering/using unique email address.
- * We can don't have to include all the columns in the partition by since we only need to check for duplicate email address.
+ * For example, if there is a data validation in the customers table by only registering/using unique email address. We can don't have to include all the columns in the partition by since we only need to check for duplicate email address.
  */
 SELECT
     *,
@@ -185,7 +184,7 @@ LIMIT 10;
 ##### ##### #####
 -- 4. Standardize data
 /*
- * REGEXP pattern used for `REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';`
+ * REGEXP pattern use for `REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';`
  * ^        = Start of the string (ensures the match starts from the first character)
  * [0-9]{4} = Matches exactly four digits (representing the year)
  * -        = Matches a literal hyphen/dash as date separator
@@ -194,8 +193,7 @@ LIMIT 10;
  * [0-9]{2} = Matches exactly two digits (representing the month or day)
  * $        = End of the string (ensures nothing follows the month or day)
  */
-
--- Checking any inconsistency with the date format. We can use the YYYY-MM-DD format for all the tables.
+# Checking any inconsistency with the date format. We can use the YYYY-MM-DD format for all the tables.
 SELECT
     *
 FROM
@@ -204,11 +202,95 @@ WHERE
     registration_date NOT REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
 -- RETURNS ALL the DATA that doesn't MATCH WITH the REGEXP (YYYY-MM-DD).
 
+/*
+ * Some customers have honorifics (Mr.,Ms.,Mrs.,Dr.) and degree (MD, DDS, DVM).
+ * Since doesn't impact our reporting, we don't need to update the customer names.
+ * But if there's a business requirement to format them, refer to the queries below.
+ */
 
+/*
+ * REGEXP breakdown
+ * '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)'
+ * ^                            = Start of the string
+ * (Dr\\.|Mrs\\.|Ms\\.|Mr\\.)   = Search for Dr., Mrs., Ms., and Mr.. The \\. is to character escape the dot.
+ * | (pipe)                     = It means OR condition
+ * We can omit the ^, it will return as True even if the search instance is in the middle (ie. John Dr. Doe)
+ */
+SELECT
+    c.customer_id,
+    c.customer_name,
+    CASE
+        WHEN RIGHT(SUBSTRING_INDEX(c.customer_name, ' ', 1),
+        1) <> '.' THEN NULL
+        ELSE SUBSTRING_INDEX(c.customer_name, ' ', 1)
+    END AS honorifics,
+    CASE
+        WHEN SUBSTRING_INDEX(c.customer_name, ' ', -1) NOT IN ('MD', 'DDS', 'DVM') THEN NULL
+        ELSE SUBSTRING_INDEX(c.customer_name, ' ', -1)
+    END AS prof_degree
+FROM
+    customer c
+WHERE
+    customer_name REGEXP '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)'
+    OR customer_name REGEXP '\\s(MD|DDS|DVM)$';
+
+# DML logic testing
+DROP TEMPORARY TABLE IF EXISTS customer_temp;
+
+CREATE TEMPORARY TABLE customer_temp AS
+
+/*
+ * REGEXP breakdown
+ * \\s = Matches any whitespace character (spaces, tabs, newlines)
+ * * = Quantifier, matches zero or more occurrences of the preceding character/class (\s in our example)
+ *
+ * \\s = "Dr.John" <--- "Dr." will not be remove since \\s means there should be a whitespace between the identifier and the next string
+ * \\s* = "Dr.John" <--- "Dr." will be removed since \\s* means whitespace with "zero or more times".
+ */
+UPDATE
+    customer_temp c
+SET
+    customer_name = REGEXP_REPLACE(
+        REGEXP_REPLACE(customer_name, '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)\\s*', ''), -- Remove prefix
+        '\\s*(MD|DDS|DVM)\\s*$', '' -- Remove suffix only if at the end
+    )
+WHERE
+    customer_name REGEXP '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)'
+    OR customer_name REGEXP '\\s(MD|DDS|DVM)$';
+
+-- Apply the DML to actual table
+CREATE TABLE it_sales.customer_bak_2025_02_05 AS
 SELECT
     *
 FROM
-    orders
+    it_sales.customer;
+
+UPDATE
+    customer c
+SET
+    customer_name = REGEXP_REPLACE(
+        REGEXP_REPLACE(customer_name, '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)\\s*', ''), -- Remove prefix
+        '\\s*(MD|DDS|DVM)\\s*$', '' -- Remove suffix only if at the end
+    )
 WHERE
-    order_date NOT REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
--- RETURNS ALL the DATA that doesn't MATCH WITH the REGEXP (YYYY-MM-DD).
+    customer_name REGEXP '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)'
+    OR customer_name REGEXP '\\s(MD|DDS|DVM)$';
+
+-- Verify the update. There should be zero result.
+SELECT
+    c.customer_id,
+    c.customer_name,
+    CASE
+        WHEN RIGHT(SUBSTRING_INDEX(c.customer_name, ' ', 1),
+        1) <> '.' THEN NULL
+        ELSE SUBSTRING_INDEX(c.customer_name, ' ', 1)
+    END AS honorifics,
+    CASE
+        WHEN SUBSTRING_INDEX(c.customer_name, ' ', -1) NOT IN ('MD', 'DDS', 'DVM') THEN NULL
+        ELSE SUBSTRING_INDEX(c.customer_name, ' ', -1)
+    END AS prof_degree
+FROM
+    customer c
+WHERE
+    customer_name REGEXP '^(Dr\\.|Mrs\\.|Ms\\.|Mr\\.)'
+    OR customer_name REGEXP '\\s(MD|DDS|DVM)$';
