c8086783cc8daf3630a3e1b57bf1d855a18afb15
lemirser <serzoleandro@gmail.com>
2025-02-06 19:29:37 +0800

    Objectives for data enrichment

---
+++ [4] Data_enrichment.sql
@@ -0,0 +1,22 @@
+-- Version 1.0, Last Modified: 2025-02-06
+-- This script is for data transformation and enrichment. Transform data into meaningful formats
+
+
+/*
+ * Objectives
+ * 1. Create Total Revenue KPI to display the revenue Yearly and/or Monthly basis.
+ * 1a. Total Revenue per product
+ * 2. Sales Growth Rate to check if the sales per Year or Month fluctuates
+ * 3. Average Revenue Per User to help measure customer value and pricing effectiveness
+ * 4. Products Sales to see which products have the hightest sales record.
+ * 5. Gross profit margin
+ * 6. Net Profit margin
+ * 7. Net profit vs net revenue vs net cost
+ * 8. Most Payment method used
+ * 9. Delivered, Canceled, and pending orders comparison
+ * 10. Customer retention rate
+ */
+
+
+USE it_sales;
+
