43a078ac5bc2416adc103906f8e4d72649814843
lemirser <serzoleandro@gmail.com>
2025-02-08 23:46:53 +0800

    Yearly and monthly sales growth

--- [4] Data_analysis.sql
+++ [4] Data_analysis.sql
@@ -1,4 +1,4 @@
--- Version 1.4, Last Modified: 2025-02-08
+-- Version 1.5, Last Modified: 2025-02-08
 -- This script is for data transformation and enrichment. Transform data into meaningful formats
 
 
@@ -6,7 +6,7 @@
  * Objectives
  * [X -- line 23] 1. Create Total Revenue KPI to display the revenue Yearly and/or Monthly basis.
  * [X -- line 56] 1a. Total Revenue per product
- * 2. Sales Growth Rate to check if the sales per Year or Month fluctuates
+ * [X -- line 110] 2. Sales Growth Rate to check if the sales per Year or Month fluctuates
  * 3. Average Revenue Per User to help measure customer value and pricing effectiveness
  * 4. Products Sales to see which products have the hightest sales record.
  * 5. Gross profit margin
@@ -105,3 +105,69 @@ WHERE
     AND o.status = 'Completed'
 GROUP BY 1,2
 ORDER BY 1;
+
+
+##### ##### #####
+-- 2. Sales Growth Rate to check if the sales per Year or Month fluctuates
+
+SELECT * FROM orders;
+
+/*
+ * The query displays the difference in sales between each month (current and the previous months).
+ * This can tell us if there is growth in our sales each month.
+ */
+WITH monthly_sales AS (
+SELECT
+        DATE_FORMAT(order_date, '%Y-%m') AS order_month,
+        SUM(o.unit_price) AS total_sales
+FROM
+    orders o
+WHERE
+    o.status = 'Completed'
+    AND o.has_order_details = 1
+GROUP BY
+    order_month
+)
+SELECT
+    curr_orders.order_month AS curr_order_month,
+    prev_orders.order_month AS prev_order_month,
+    curr_orders.total_sales AS curr_sales,
+    prev_orders.total_sales AS prev_sales,
+    ROUND(((curr_orders.total_sales - prev_orders.total_sales) / prev_orders.total_sales) * 100, 2) AS sales_growth_percentage
+FROM
+    monthly_sales curr_orders
+LEFT JOIN monthly_sales prev_orders
+    ON
+    prev_orders.order_month = DATE_FORMAT(STR_TO_DATE(DATE_SUB(CONCAT(curr_orders.order_month, '-01'), INTERVAL 1 MONTH), '%Y-%m-%d'), '%Y-%m')
+ORDER BY
+    curr_orders.order_month ASC;
+
+
+/*
+ * The query below shows us that there's a 45.8% in sales growth between the year of 2023 and 2024.
+ */
+WITH yearly_sales AS (
+SELECT
+        YEAR(order_date) AS order_year,
+        SUM(o.unit_price) AS total_sales
+FROM
+    orders o
+WHERE
+    o.status = 'Completed'
+    AND o.has_order_details = 1
+GROUP BY
+    order_year
+)
+SELECT
+    curr_orders.order_year AS curr_order_year,
+    prev_orders.order_year AS prev_order_year,
+    curr_orders.total_sales AS curr_sales,
+    prev_orders.total_sales AS prev_sales,
+    ROUND(((curr_orders.total_sales - prev_orders.total_sales) / prev_orders.total_sales) * 100, 2) AS sales_growth_percentage
+FROM
+    yearly_sales curr_orders
+LEFT JOIN yearly_sales prev_orders
+    ON
+    prev_orders.order_year = YEAR(STR_TO_DATE(DATE_SUB(CONCAT(curr_orders.order_year, '-01-01'), INTERVAL 1 Year), '%Y-%m-%d'))
+ORDER BY
+    curr_orders.order_year ASC;
